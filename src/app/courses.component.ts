bd644127122889b15fe6f21df8d6d8d7689bfea9
saeedek <ektesabi.saeed@gmail.com>
2019-06-27 08:17:43 +0200

    used 2-way binding variable

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,17 +4,20 @@ import { CoursesService } from './courses.service';
 @Component({
     selector : 'courses',
     template : `
-        <input #email (keyup.enter)="onKeyUp(email.value)" />
+        <input [(ngModel)]="email" (keyup.enter)="onKeyUp()" />
         
         <button (click)="onSave($event)" class="btn btn-primary">Save</button>    
     `
 })
+//<input [value]="email" (keyup.enter)="email = $event.target.value; onKeyUp()" /> bad way for 2-way binding
+//<input #email (keyup.enter)="onKeyUp(email.value)" /> template variables
 //<input (keyup.enter)="onKeyUp($event)" /> //bad way to get the value of the field
 //<button class="btn btn-primary" [class.active]="isActive">Save</button>  to add or remove a class
 //<button class="btn btn-primary" [style.backgroundColor]="isActive ? 'blue' : 'white'">Save</button>  //style binding
 //<button class="btn btn-primary" [attr.colspan]="2">Save</button>  //to bind attributes that are not in the dom
 //<input (keyup)="onKeyUp($event)" />
 export class CoursesComponent{
+    email = "Hello";
     title = "List of courses";
     imageUrl = "http://lorempixel.com/400/200"  
     isActive = true;  
@@ -22,11 +25,18 @@ export class CoursesComponent{
         $event.stopPropagation(); //to stop the event to bubble up
         console.log("button was clicked", $event);
     }
-    onKeyUp(email){
+    onKeyUp(){
         // if($event.keyCode === 13){
         //     console.log("Enter was pressed")
         // }
-        console.log("the email is "+ email)
+        console.log("the email is "+ this.email)
 
     }
+    // onKeyUp(email){
+    //     // if($event.keyCode === 13){
+    //     //     console.log("Enter was pressed")
+    //     // }
+    //     console.log("the email is "+ email)
+
+    // }
 }
