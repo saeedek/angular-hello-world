a4b683b1c93c54e7fce83833dc998a8f89a2e490
saeedek <ektesabi.saeed@gmail.com>
2019-06-27 07:59:33 +0200

    used template variables

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,10 +4,12 @@ import { CoursesService } from './courses.service';
 @Component({
     selector : 'courses',
     template : `
-        <input (keyup.enter)="onKeyUp()" />
+        <input #email (keyup.enter)="onKeyUp(email.value)" />
+        
         <button (click)="onSave($event)" class="btn btn-primary">Save</button>    
     `
 })
+//<input (keyup.enter)="onKeyUp($event)" /> //bad way to get the value of the field
 //<button class="btn btn-primary" [class.active]="isActive">Save</button>  to add or remove a class
 //<button class="btn btn-primary" [style.backgroundColor]="isActive ? 'blue' : 'white'">Save</button>  //style binding
 //<button class="btn btn-primary" [attr.colspan]="2">Save</button>  //to bind attributes that are not in the dom
@@ -20,11 +22,11 @@ export class CoursesComponent{
         $event.stopPropagation(); //to stop the event to bubble up
         console.log("button was clicked", $event);
     }
-    onKeyUp(){
+    onKeyUp(email){
         // if($event.keyCode === 13){
         //     console.log("Enter was pressed")
         // }
-        console.log("enter was pressed")
+        console.log("the email is "+ email)
 
     }
 }
