de829251809c28ba863a4f84a8c32fb169a1935b
Saeed <ektesabi.saeed@gmail.com>
2019-06-28 12:22:38 +0200

    made simple output event emitter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,7 @@ export class AppComponent {
     title : "Title",
     isFavorite : true
   }
+  onFavoriteChanged(){
+    console.log("Favorite changed");
+  }
 }
--- src/app/favorite-star/favorite-star.component.ts
+++ src/app/favorite-star/favorite-star.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'favorite-star',
@@ -9,12 +9,14 @@ import { Component, OnInit, Input } from '@angular/core';
 export class FavoriteStarComponent implements OnInit {
   @Input('is-favorite') isFavorite: boolean; //gave it a nickname so it doesn't break the contract in this com's html and other consumers
   //Input() isFavorite = false; one way to declare input
+  @Output() change =new EventEmitter();
   constructor() { }
 
   ngOnInit() {
   }
   onClick(){
     this.isFavorite = !this.isFavorite;
+    this.change.emit();
   }
 
 }
