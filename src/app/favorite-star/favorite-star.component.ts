aa19a562f980aee042e4c2c770ff0bf7a45337b1
Saeed <ektesabi.saeed@gmail.com>
2019-06-28 12:06:38 +0200

    simple input properties

--- src/app/favorite-star/favorite-star.component.ts
+++ src/app/favorite-star/favorite-star.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'favorite-star',
   templateUrl: './favorite-star.component.html',
   styleUrls: ['./favorite-star.component.css']
+  //inputs: ["isFavorite"] bad way to make input properties.
 })
 export class FavoriteStarComponent implements OnInit {
-  isFavorite = false;
+  @Input() isFavorite: boolean;
+  //Input() isFavorite = false; one way to declare input
   constructor() { }
 
   ngOnInit() {
