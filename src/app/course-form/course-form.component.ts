8629a463030cbbce0e2750e12629427cd8902ff9
saeedek <ektesabi.saeed@gmail.com>
2019-07-02 13:53:47 +0200

    implemented course-form

--- src/app/course-form/course-form.component.ts
+++ src/app/course-form/course-form.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-course-form',
+  selector: 'course-form',
   templateUrl: './course-form.component.html',
   styleUrls: ['./course-form.component.css']
 })
-export class CourseFormComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class CourseFormComponent  {
+  categories=[
+    {id: 1, name: "Development"},
+    {id: 2, name: "Art"},
+    {id: 3, name: "Languages"}
+  ]
+  
 
 }
