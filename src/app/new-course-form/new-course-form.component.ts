2a4c0f309b0ab88a2e99d150c032243ef1b6dcd0
Saeed <ektesabi.saeed@gmail.com>
2019-07-03 15:15:32 +0200

    used FormBuilder to build the reactive form

--- src/app/new-course-form/new-course-form.component.ts
+++ src/app/new-course-form/new-course-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormArray, FormControl } from '@angular/forms';
+import { FormGroup, FormArray, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'new-course-form',
@@ -7,18 +7,46 @@ import { FormGroup, FormArray, FormControl } from '@angular/forms';
   styleUrls: ['./new-course-form.component.css']
 })
 export class NewCourseFormComponent {
-  form = new FormGroup({
-    topics: new FormArray([])
-  })
-  addTopic(topic: HTMLInputElement){
-    this.topics.push(new FormControl(topic.value))
-    topic.value = ""
+  // form = new FormGroup({
+  //   name: new FormControl('',Validators.required),
+  //   contact: new FormGroup({
+  //     email: new FormControl(),
+  //     phone: new FormControl()
+  //   }),
+  //   topics: new FormArray([])
+  // })
+  constructor(fb: FormBuilder){
+    fb.group({
+      name: ['',Validators.required],
+      contact : fb.group({
+        email: [],
+        phone: []
+      }),
+      topics : fb.array([])
+    })
   }
-  get topics(){
-    return this.form.get('topics') as FormArray;
-  }
-  removeTopic(topic : FormControl){
-    let index = this.topics.controls.indexOf(topic);
-    this.topics.removeAt(index);
-  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // addTopic(topic: HTMLInputElement){
+  //   this.topics.push(new FormControl(topic.value))
+  //   topic.value = ""
+  // }
+  // get topics(){
+  //   return this.form.get('topics') as FormArray;
+  // }
+  // removeTopic(topic : FormControl){
+  //   let index = this.topics.controls.indexOf(topic);
+  //   this.topics.removeAt(index);
+  // }
 }
