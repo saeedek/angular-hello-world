31943da4877b70ac5f1148d071ebcd8a4fdd1f79
Saeed <ektesabi.saeed@gmail.com>
2019-07-04 22:21:37 +0200

    simple PUT/PATCH usage

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -28,5 +28,17 @@ export class PostsComponent {
       console.log(newPost)
     })
   }
+  updatePost(post){
+    // this.client.put(this.url,
+    //   JSON.stringify(post)
+    //   )
+    this.client.patch(this.url + '/' + post.id,
+      JSON.stringify({
+        isRead : true
+      })
+      ).subscribe(response =>{
+        console.log(response);
+      })
+  }
 
 }
